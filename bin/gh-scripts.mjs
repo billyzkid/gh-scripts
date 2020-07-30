5a82cdf395593134f85dbe4531e72a6e54788391
Will Allan <will.allan@disneystreaming.com>
2020-07-29 19:34:50 -0700

    Add .env support

--- bin/gh-scripts.mjs
+++ bin/gh-scripts.mjs
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 
+import dotenv from "dotenv"
 import * as authors from "../lib/commands/authors.mjs";
 import * as changelog from "../lib/commands/changelog.mjs";
+import * as github from "../lib/common/github.mjs";
+import * as utilities from "../lib/common/utilities.mjs";
+
+dotenv.config({ path: utilities.findFileUp(".env") });
+github.config({ token: process.env.GITHUB_API_TOKEN });
 
 const command = process.argv[2];
 const args = process.argv.slice(3);
